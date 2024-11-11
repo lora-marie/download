8f73b8058a1bd6f68e4b41f375ac4b2ea924352e
lora-marie <32797134+lora-marie@users.noreply.github.com>
2024-11-11 12:17:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 <p>Click on the "Choose File" button to upload a file:</p>
 
 <form action="/upload_file.php">
-  <input type="file" id="myFile" name="filename">
-  <input type="submit">
+  <input type="file" id="myFile" name="filename"/>
+  <input type="submit"/>
 </form>
 
 </body>
