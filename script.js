f2746dabbf84e597e9134dcd70f8cd258a667680
lora-marie <32797134+lora-marie@users.noreply.github.com>
2024-11-21 14:47:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const tableau_fichier = {
+  classe_1_pro_hocv: '',
+  classe_1_pro_ap: 'eval_risque_alcool.ods',
+  classe_2_general: '',
+  classe_2_pro_njpf: '',
+  classe_2_pro_pegc: ''
+}
+
+
 function selection_classe(){
   var classe_nom = document.getElementById("select_classe").value;
-  console.log(classe_nom);
+  let fichier_nom = tableau_fichier.classe_nom;
+  console.log(fichier_nom);
+  
+}
+function telecharger_fichier(){
   
 }
